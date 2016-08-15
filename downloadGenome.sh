630ac6d2bd3b2183f28e91a7b28487c413e4f348
atmaivancevic <atma.ivancevic@adelaide.edu.au>
2016-08-15 16:11:25 +0930

    update with comments

--- downloadGenome.sh
+++ downloadGenome.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+# Shows how to download a publicly available genome from NCBI Assembly.
+# To download multiple genomes at once, 
+# create a for loop with the changing parameters being:
+# directory name (here given as the species name, e.g. Elephantulus.edwardii)
+# genome name (e.g. EleEdw1.0.fa)
+# and ftp link (see below)
+
 # Move to allocated directory
 cd Genomes/
 
